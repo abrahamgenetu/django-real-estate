a4d1f6c28300492c07ffc1d0bf9b7fba4118a840
abrishg73@gmail.com <abrishg73@gmail.com>
2022-06-12 01:18:27 +0300

    ratings view.py configuratons

--- real_estates/urls.py
+++ real_estates/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('supersecret/', admin.site.urls),
     path('api/v1/auth/', include("djoser.urls")),
     path('api/v1/auth/', include("djoser.urls.jwt")),
-    path('api/v1/profile/', include('apps.profiles.urls'))
+    path('api/v1/profile/', include('apps.profiles.urls')),
+    path('api/v1/properties/', include('apps.properties.urls')),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
 admin.site.site_header = "Real Estate Admin"

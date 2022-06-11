77243529c17801bfd1bf48914ba1a3c735e9661e
abrishg73@gmail.com <abrishg73@gmail.com>
2022-06-11 13:12:09 +0300

    chore:configure django loggings

--- real_estates/settings/base.py
+++ real_estates/settings/base.py
@@ -1,5 +1,9 @@
+from http import server
 import environ
 from pathlib import Path
+import logging
+import logging.config
+from django.utils.log import DEFAULT_LOGGING
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -129,3 +133,39 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+logger = logging.getLogger(__name__)
+
+LOG_LEVEL = "INFO"
+
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "console": {
+                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+            },
+            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "console",
+            },
+            "file": {
+                "level": "INFO",
+                "class": "logging.FileHandler",
+                "formatter": "file",
+                "filename": "logs/real_estate.log",
+            },
+            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
+        },
+        "loggers": {
+            "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
+            "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
+            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
+        },
+    }
+)
--- real_estates/settings/development.py
+++ real_estates/settings/development.py
@@ -1,12 +1,12 @@
 from .base import *
 
 DATABASES = {
-    "default": {
-        "ENGINE": env("POSTGRES_ENGINE"),
-        "NAME": env("POSTGRES_DB"),
-        "USER": env("POSTGRES_USER"),
-        "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": env("PG_HOST"),
-        "PORT": env("PG_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Real_estate',
+        'USER': 'abraham', #default user name
+        'PASSWORD': 'yoratorad@abraham3',
+        'HOST': '127.0.0.1', # default host
+        'PORT': '5432', # default postgres port
     }
 }
